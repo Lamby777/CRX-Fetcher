99229957b79543c322b3efa1b737b97dff5b7dbb
Dex <sheepareawesome777@gmail.com>
2022-10-18 12:38:10 -0400

    Added HTML and basic styling

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+*   More user-friendly rewrite of CRX-R using the Fetch API
+*   to download off of clients2.
+*
+*   I drank the Kool-Aid. Oh yeahhhhh!
+*/
+
+"use strict";
+
+const submitButton = document.getElementById("submit");
